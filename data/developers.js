8101ea38d8b77ffa25439b3095bb15f5147f583b
Anmalas <akulupaya23@gmail.com>
2026-09-04 13:06:59 +0700

    Delete data/developers.js

--- data/developers.js
+++
@@ -1,5 +0,0 @@
-// Tambahkan developer baru di dalam array ini jika nanti diperlukan.
-// Format: { name: 'Nama', role: 'Role', photo: 'assets/team/nama-foto.jpg' }
-export const developers = [
-  { name: 'Anma', role: 'Founder & Developer', photo: 'assets/team/anggota-1.jpg' },
-];
